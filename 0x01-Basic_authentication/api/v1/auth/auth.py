f0ef75058f9436207a56ae1d993c2a2e719850d0
rallmac <tobijahekperikpe@gmail.com>
2024-11-13 10:02:35 +0100

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,16 @@ class Auth:
         """Determines if a given path requires authentication
         currently returns false as a placeholder
         """
-        return False
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+
+        normalized_path = path if path.endswith('/') else path + '/'
+        normalized_excluded_paths = []
+        for p in excluded_paths:
+            normalized_excluded_paths.append(p if p.endswith('/') else p + '/')
+        return normalized_path not in normalized_excluded_paths
 
     def authorization_header(self, request=None) -> str:
         """Retrieves the authorization header from a request
