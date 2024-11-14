5372730e3602538a8f69bb9be32d38102d0f7ab7
rallmac <tobijahekperikpe@gmail.com>
2024-11-14 08:43:35 +0100

    task 11 corrected basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,6 +9,7 @@ import base64
 
 class BasicAuth(Auth):
     """Basic class that inherits from Auth"""
+
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """Returns the Base64 part of the Authorization header
@@ -30,9 +31,7 @@ class BasicAuth(Auth):
         if not isinstance(base64_authorization_header, str):
             return None
         try:
-            decoded_bytes = base64.b64decode(
-                base64_authorization_header
-            )
+            decoded_bytes = base64.b64decode(base64_authorization_header)
             return decoded_bytes.decode('utf-8')
         except Exception:
             return None
@@ -76,13 +75,3 @@ class BasicAuth(Auth):
             return None
 
         return user
-
-    def extract_base64_authorization_header(
-        self, authorization_header: str
-    ) -> str:
-        if (authorization_header is None or
-                not isinstance(authorization_header, str)):
-            return None
-        if not authorization_header.startswith("Basic "):
-            return None
-        return authorization_header.split(" ", 1)[1]
