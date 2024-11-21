d47fce6aff755504fad1f6c23eec65bf568daff4
rallmac <tobijahekperikpe@gmail.com>
2024-11-21 11:11:35 +0100

    task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 """This function returns a log message obfuscated"""
 
+import re
 
-def filter_datum():
+
+def filter_datum(fields, redaction, message, seperator):
+    """Obfuscates specified fields in a log message"""
+    escaped_fields = '|'.join(re.escape(field) for field in fields)
+    pattern = f"({escaped_fields})=.*?{re.escape(seperator)}"
+    return re.sub(
+        pattern,
+        lambda m: f"{m.group(1)}={redaction}{seperator}", message)
